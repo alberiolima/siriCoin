53e29c877437268a99137d90ff02e6acb29e62ff
Alberio Lima <alberio@dadox.com.br>
2022-04-18 15:35:25 -0300

    Atualizado minerador para trabalhar com o esp32

--- SiriCoinSerialMiner/SiriCoinSerialMiner.py
+++ SiriCoinSerialMiner/SiriCoinSerialMiner.py
@@ -8,8 +8,7 @@ from colorama import Fore
 #change usb-serial port (baud=115200)
 serialPort = "/dev/ttyUSB0"
 
-#change user_id
-minerAddr = "0x0E9b419F7Cd861bf86230b124229F9a1b6FF9674"
+tempoTrabalhar = 20
 
 class SignatureManager(object):
     def __init__(self):
@@ -54,7 +53,7 @@ class SiriCoinMiner(object):
         self.acct = w3.eth.account.from_key(self.priv_key)
         self.messages = b"null"
         
-        self.timestamp = time.time()
+        self.timestamp = int(time.time())
         _txs = self.requests.get(f"{self.node}/accounts/accountInfo/{self.acct.address}").json().get("result").get("transactions")
         self.lastSentTx = _txs[len(_txs)-1]
         self.refresh()
@@ -66,21 +65,22 @@ class SiriCoinMiner(object):
         self.lastBlock = info["lastBlockHash"]
         _txs = self.requests.get(f"{self.node}/accounts/accountInfo/{self.acct.address}").json().get("result").get("transactions")
         self.lastSentTx = _txs[len(_txs)-1]
-        self.timestamp = time.time()
+        self.timestamp = int(time.time())
         self.nonce = 0
     
     def submitBlock(self, blockData):
         data = json.dumps({"from": self.acct.address, "to": self.acct.address, "tokens": 0, "parent": self.lastSentTx, "blockData": blockData, "epoch": self.lastBlock, "type": 1})
         tx = {"data": data}
-        tx = self.signer.signTransaction(self.priv_key, tx)
+        tx = self.signer.signTransaction(self.priv_key, tx)        
+        self.refresh()
         txid = self.requests.get(f"{self.node}/send/rawtransaction/?tx={json.dumps(tx).encode().hex()}").json().get("result")[0]
-        print(f"SYS{Fore.GREEN} Mined block {blockData['miningData']['proof']}")
+        print(f"SYS Mined block {blockData['miningData']['proof']}")
         print(f"Submitted in transaction {txid}")
         return txid
 
     def beaconRoot(self):        
         messagesHash = w3.keccak(self.messages)        
-        bRoot = w3.soliditySha3(["bytes32", "uint256", "bytes32","address"], [self.lastBlock, int(self.timestamp), messagesHash, self.rewardsRecipient]) # parent PoW hash (bytes32), beacon's timestamp (uint256), hash of messages (bytes32), beacon miner (address)
+        bRoot = w3.soliditySha3(["bytes32", "uint256", "bytes32","address"], [self.lastBlock, self.timestamp, messagesHash, self.rewardsRecipient]) # parent PoW hash (bytes32), beacon's timestamp (uint256), hash of messages (bytes32), beacon miner (address)
         return bRoot.hex()
 
     def formatHashrate(self, hashrate):
@@ -93,53 +93,51 @@ class SiriCoinMiner(object):
         elif hashrate < 1000000000000:
             return f"{round(hashrate/1000000000, 2)}GH/s"
             
-    def startMining(self):        
-        ser  = serial.Serial(f"{serialPort}", baudrate=115200, timeout=2.5)
-        print(f"SYS {Fore.GREEN}Started mining for {minerAddr}")
-        proof = ""
+    def startMining(self):
+        print(f"SYS Started mining for {minerAddr}")
+        self.refresh()
+        ser  = serial.Serial(f"{serialPort}", baudrate=115200, timeout=2.5)        
+        proof = "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"        
         self_lastBlock = ""
         while True:
-            self.refresh()
-            bRoot = self.beaconRoot()
+            self.refresh()                        
             if (self_lastBlock != self.lastBlock):
                 self_lastBlock = self.lastBlock
-                print(f"lastBlock: {self_lastBlock}")
-            
-            #Envia trabalho para trabalhador via porta serial: bRoot e target
-            ddata = f"{bRoot},{self.target}\n"            
-            ser.write(ddata.encode('ascii'))
+                print(f"lastBlock  : {self_lastBlock}")
+                print(f"target     : {self.target}")
+                print(f"difficulty : {self.difficulty}")
+            #self.timestamp = 1650303688
+            bRoot = self.beaconRoot()                        
+            ddata = f"{bRoot},{self.target},{tempoTrabalhar}\n"
             ser.flush()
-            
-            sucesso = False
+            ser.write(ddata.encode('ascii'))            
             recebido = ""
-            tempo_decorrido = 30
+            tempo_decorrido = tempoTrabalhar
             tinicial = time.time()
-            while (time.time() - tinicial) < 120:
-				#recebe dados via porta serial
+            q_bytes = 0;
+            while (time.time() - tinicial) < (tempoTrabalhar * 2):                
                 if (ser.in_waiting>0):
                     byte_lido = ser.read()
+                    q_bytes = q_bytes + 1
                     if (byte_lido == b'\n'):						
                         ress = recebido.split(',')
-                        self.nonce = int(ress[0])
-                        tempo_decorrido = int(ress[1]) * 0.000001
-                        proof = ress[2]
+                        self.nonce = int(ress[0].rstrip())
+                        tempo_decorrido = round(int(ress[1].rstrip()) * 0.000001)
+                        proof = ress[2].rstrip()
                         recebido = ""
-                        if (len(proof)>20):
-                            sucesso = True
-                        print(f"SYS {Fore.YELLOW}Last {round(tempo_decorrido,2)} seconds hashrate : {self.formatHashrate((self.nonce / tempo_decorrido))}")
+                        print(f"SYS {self.timestamp} Last {round(tempo_decorrido,2)} seconds hashrate : {self.formatHashrate((self.nonce / tempo_decorrido))}")
+                        if (q_bytes>32):
+                            print(f"bRoot: {bRoot}")
+                            self.submitBlock({"miningData" : {"miner": self.rewardsRecipient,"nonce": self.nonce,"difficulty": self.difficulty,"miningTarget": self.target,"proof": proof}, "parent": self.lastBlock,"messages": self.messages.hex(), "timestamp": self.timestamp, "son": "0000000000000000000000000000000000000000000000000000000000000000"})
+                            print("")
+                        q_bytes = 0
                         break
                     else:
-                        recebido = recebido + byte_lido.decode("utf-8") 
-                else:
-                    info_ = self.requests.get(f"{self.node}/chain/miningInfo").json().get("result")
-                    if (self_lastBlock != info_["lastBlockHash"]):
-                        break
-                # Envia bloco minerado
-                if(sucesso):
-                    self.submitBlock({"miningData" : {"miner": self.rewardsRecipient,"nonce": self.nonce,"difficulty": self.difficulty,"miningTarget": self.target,"proof": proof}, "parent": self.lastBlock,"messages": self.messages.hex(), "timestamp": self.timestamp, "son": "0000000000000000000000000000000000000000000000000000000000000000"})
-                    break
+                        recebido = recebido + byte_lido.decode("utf-8")
 
 if __name__ == "__main__":
     #minerAddr = input("Enter your SiriCoin address : ")    
-    miner = SiriCoinMiner("https://siricoin-node-1.dynamic-dns.net:5005/", minerAddr)
+    #miner = SiriCoinMiner("https://siricoin-node-1.dynamic-dns.net:5005/", minerAddr)
+    minerAddr = "0x0E9b419F7Cd861bf86230b124229F9a1b6FF9674"
+    miner = SiriCoinMiner("http://138.197.181.206:5005/", minerAddr)
     miner.startMining()
